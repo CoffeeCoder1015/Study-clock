ab50eff9f3453826187a670617cff2a0c9b6aa0e
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2025-05-27 02:53:46 +1000

    clock displays

--- study-clock/src/app/page.tsx
+++ study-clock/src/app/page.tsx
@@ -1,12 +1,13 @@
 import styles from "@/app/ui/home.module.css";
+import { Clock } from "@/components/clock";
 
 export default function Home() {
   return (
     <div className={styles.main}>
-      Start
-      Study
-      Break
-      Hello
+      <div>
+        <Clock label="Study clock" time={{hours:0,minutes:0,seconds:0}}/>
+        <Clock label="Break clock" time={{hours:0,minutes:0,seconds:0}}/>
+      </div>
     </div>
   );
 }
---
+++ study-clock/src/components/clock.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+interface TimerProps {
+  label: string
+  time: { hours: number; minutes: number; seconds: number }
+}
+
+export function Clock({ label, time}: TimerProps)  {
+  const formatTime = (num: number) => num.toString().padStart(2, "0")
+  return(
+    <div>
+      <div className="text-2xl mb-4">{label}</div>
+      <div className="text-6xl font-mono">
+        <span id="hr">{formatTime(time.hours)}</span>:
+        <span id="min">{formatTime(time.minutes)}</span>:
+        <span id="sec">{formatTime(time.seconds)}</span>
+      </div>
+    </div>
+  )
+}
