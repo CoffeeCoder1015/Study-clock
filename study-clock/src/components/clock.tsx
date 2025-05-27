2fe6e76c45c50c782b4b6983bc520ad8777d9529
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2025-05-28 04:13:37 +1000

    border styling

--- study-clock/src/components/clock.tsx
+++ study-clock/src/components/clock.tsx
@@ -10,7 +10,7 @@ export function Clock({ label, time}: TimerProps)  {
   return(
     <div>
       <div className="text-2xl mb-4">{label}</div>
-      <div className="text-8xl font-mono border-solid border-4 rounded-md">
+      <div className="text-8xl font-mono border-solid border-4 rounded-md focus-within:border-(--input-hover)">
         <span id="hr">{formatTime(time.hours)}</span>:
         <span id="min">{formatTime(time.minutes)}</span>:
         <span id="sec">{formatTime(time.seconds)}</span>
