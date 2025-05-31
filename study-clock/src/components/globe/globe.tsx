a555c7f7d1e8daa677e325e32a7e2daccb9a1571
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2025-06-01 03:47:42 +1000

    basic setup

--- study-clock/src/components/globe/globe.tsx
+++ study-clock/src/components/globe/globe.tsx
@@ -1,5 +1,39 @@
+import { useEffect, useRef } from  "react"
+import * as THREE from "three";
+
 export function Globe(){
-    return ( <div>
-        🌍
-    </div> )
+    const renderRef = useRef<HTMLDivElement | null>(null)
+
+    useEffect(() => {
+        const currentMount = renderRef.current
+        if (!currentMount){
+            return
+        }
+        
+        const scene  = new THREE.Scene()
+        const camera = new THREE.PerspectiveCamera(75, currentMount.clientWidth / currentMount.clientHeight, 0.1, 1000)
+        camera.position.z = 3
+        
+        const renderer = new THREE.WebGLRenderer({alpha:true})
+        renderer.setClearColor(0x000000, 0)
+        renderer.setSize(currentMount.clientWidth,currentMount.clientHeight)
+        currentMount.appendChild(renderer.domElement)
+
+        const geometry = new THREE.BoxGeometry();
+        const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+        const cube = new THREE.Mesh(geometry, material);
+        scene.add(cube);
+
+        function animate(){
+            requestAnimationFrame(animate)
+            renderer.render(scene,camera)
+        }
+        animate()
+
+        return () => {
+            currentMount.removeChild(renderer.domElement)
+        }
+    },[])
+
+    return ( <div ref={renderRef} className="w-2xl h-114"></div> )
 }
