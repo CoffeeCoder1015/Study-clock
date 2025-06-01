193eb69935d2b1b886dbc313ed439694c7f9a931
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2025-06-01 18:36:55 +1000

    lighting is back!

--- study-clock/public/globe/frag.glsl
+++ study-clock/public/globe/frag.glsl
@@ -1,7 +1,15 @@
 varying vec2 vertexUV;
+varying vec3 vNormal;
+varying vec3 vPosition;
 
-uniform sampler2D globeTexture;
+uniform sampler2D dayTexture;
+uniform sampler2D nightTexture;
+uniform vec3 lightPosition;
+uniform float ambientLightIntensity;
 
 void main(){
-    gl_FragColor = texture2D(globeTexture,vertexUV);
+    float intensity = dot(normalize(lightPosition),vNormal);
+    vec3 diff = lightPosition-vPosition;
+    float dist = sqrt(dot(diff,diff));
+    gl_FragColor = mix( texture2D(nightTexture,vertexUV),texture2D(dayTexture,vertexUV),intensity*dist/15.)+intensity/5.;
 }
--- study-clock/src/components/globe.tsx
+++ study-clock/src/components/globe.tsx
@@ -5,51 +5,63 @@ export function Globe(){
     const renderRef = useRef<HTMLDivElement | null>(null)
 
     useEffect(() => { 
-        async function render(){
         const currentMount = renderRef.current
-        if (!currentMount){
+        if (!currentMount) {
             return
         }
-        
-        const scene  = new THREE.Scene()
+
+        const scene = new THREE.Scene()
         const camera = new THREE.PerspectiveCamera(75, currentMount.clientWidth / currentMount.clientHeight, 0.1, 1000)
         camera.position.z = 20
-        
-        const renderer = new THREE.WebGLRenderer({alpha:true, antialias:true})
+
+        const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true })
         renderer.setClearColor(0x000000, 0)
         renderer.setPixelRatio(window.devicePixelRatio)
         renderer.setSize(currentMount.clientWidth, currentMount.clientHeight)
         currentMount.appendChild(renderer.domElement)
-
         const light = new THREE.DirectionalLight(0xffffff, 4);
         light.position.set(10, 0, 0);
         scene.add(light);
 
         const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
         scene.add(ambientLight);
-
+        
         const geometry = new THREE.SphereGeometry(10, 50, 50);
+        const material = new THREE.MeshStandardMaterial({ map: new THREE.TextureLoader().load("./globe/earth-night.jpg") });
+        const sphere = new THREE.Mesh(geometry, new THREE.ShaderMaterial());
 
-        const [vertex, fragment] = await Promise.all([
-            fetch("./globe/vert.glsl").then(res => res.text()),
-            fetch("./globe/frag.glsl").then(res => res.text())
-        ])
+        async function render() {
+            const [vertex, fragment] = await Promise.all([
+                fetch("./globe/vert.glsl").then(res => res.text()),
+                fetch("./globe/frag.glsl").then(res => res.text())
+            ])
 
-        const shader = new THREE.ShaderMaterial({
-            vertexShader: vertex,
-            fragmentShader: fragment,
-            uniforms:{
-                globeTexture:{
-                    value:new THREE.TextureLoader().load("./globe/earth-night.jpg") 
+            const shader = new THREE.ShaderMaterial({
+                vertexShader: vertex,
+                fragmentShader: fragment,
+                uniforms: {
+                    dayTexture: {
+                        value: new THREE.TextureLoader().load("./globe/earth-day.jpg")
+                    },
+                    nightTexture: {
+                        value: new THREE.TextureLoader().load("./globe/earth-night.jpg")
+                    },
+                    lightPosition: {
+                        value: new THREE.Vector3(10, 0, 0)
+                    },
+                    ambientLightIntensity: {
+                        value: 0.3
+                    }
                 }
-            }
-        })
-        console.log(vertex, fragment)
-
-        const sphere = new THREE.Mesh(geometry, shader);
-        scene.add(sphere);
+            })
+            sphere.material = shader
+            scene.add(sphere);
+        }
+        render()
 
         function animate() {
+            sphere.rotateY(1/200)
+            sphere.rotateX(1/800)
             requestAnimationFrame(animate)
             renderer.render(scene, camera)
         }
@@ -58,8 +70,6 @@ export function Globe(){
         return () => {
             currentMount.removeChild(renderer.domElement)
         }
-    }
-        render()
     },[])
 
     return ( <div ref={renderRef} className="w-2xl h-114"></div> )
