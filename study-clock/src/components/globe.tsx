4703a13934b91f2af1a0be0121f772eb12ee2b76
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2025-06-01 13:39:13 +1000

    i got the sphere back

--- study-clock/src/components/globe.tsx
+++ study-clock/src/components/globe.tsx
@@ -4,7 +4,8 @@ import * as THREE from "three";
 export function Globe(){
     const renderRef = useRef<HTMLDivElement | null>(null)
 
-    useEffect(() => {
+    useEffect(() => { 
+        async function render(){
         const currentMount = renderRef.current
         if (!currentMount){
             return
@@ -17,45 +18,44 @@ export function Globe(){
         const renderer = new THREE.WebGLRenderer({alpha:true, antialias:true})
         renderer.setClearColor(0x000000, 0)
         renderer.setPixelRatio(window.devicePixelRatio)
-        renderer.setSize(currentMount.clientWidth,currentMount.clientHeight)
+        renderer.setSize(currentMount.clientWidth, currentMount.clientHeight)
         currentMount.appendChild(renderer.domElement)
 
         const light = new THREE.DirectionalLight(0xffffff, 4);
-        light.position.set(10,0,0);
+        light.position.set(10, 0, 0);
         scene.add(light);
 
         const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
         scene.add(ambientLight);
 
-        const geometry = new THREE.SphereGeometry(10,50,50);
-        const material = new THREE.MeshStandardMaterial({ map:new THREE.TextureLoader().load("./globe/earth-night.jpg") });
+        const geometry = new THREE.SphereGeometry(10, 50, 50);
+        const material = new THREE.MeshStandardMaterial({ map: new THREE.TextureLoader().load("./globe/earth-night.jpg") });
 
-        var vertexShader = ""
-        var fragmentShader = ""
-        Promise.all([
-            fetch("./globe/frag.glsl").then(res => res.text()),
-            fetch("./globe/vert.glsl").then(res => res.text())
-        ]).then(([vertex, fragment]) => {
-            vertexShader = vertex
-            fragmentShader = fragment
-        })
+        const [vertex, fragment] = await Promise.all([
+            fetch("./globe/vert.glsl").then(res => res.text()),
+            fetch("./globe/frag.glsl").then(res => res.text())
+        ])
+
         const shader = new THREE.ShaderMaterial({
-            vertexShader: vertexShader,
-            fragmentShader: fragmentShader,
+            vertexShader: vertex,
+            fragmentShader: fragment,
         })
+        console.log(vertex, fragment)
 
-        const sphere = new THREE.Mesh(geometry, material);
+        const sphere = new THREE.Mesh(geometry, shader);
         scene.add(sphere);
 
-        function animate(){
+        function animate() {
             requestAnimationFrame(animate)
-            renderer.render(scene,camera)
+            renderer.render(scene, camera)
         }
         animate()
 
         return () => {
             currentMount.removeChild(renderer.domElement)
         }
+    }
+        render()
     },[])
 
     return ( <div ref={renderRef} className="w-2xl h-114"></div> )
