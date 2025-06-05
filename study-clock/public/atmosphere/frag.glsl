ef2661c25708a73851102d25fdd6d61134226e12
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2025-06-05 15:37:11 +1000

    basic lighting

--- study-clock/public/atmosphere/frag.glsl
+++ study-clock/public/atmosphere/frag.glsl
@@ -11,7 +11,8 @@ void main(){
     // float intensity = 1.-0.7*dot(vNormal,normalize( cameraPosition ));
     // gl_FragColor = vec4(0.13,0.3-intensity/10.,0.5,pow(intensity,1.5));
     
-    // lighting
-    float lightingIntensity = dot(lightPosition, vNormal);
-    gl_FragColor = vec4(1.)*lightingIntensity;
+    // general lighting
+    vec3 lightDirection = lightPosition-vNormal*radiusAtmosphere;
+    float lightingIntensity = dot(normalize(lightDirection), vNormal);
+    gl_FragColor = vec4(1.)+lightingIntensity;
 }
